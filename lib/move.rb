ab50db74146f19b6c9eea3e3b6e689b86ff31f64
Bryn Knowles <bryn.knowles@gmail.com>
2020-06-22 19:32:58 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,8 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
-def move(array, index, character = "X" )
-  array[0] = "X"
-end
+# def move(array, index, character = "X" )
+#   array[0] = "X"
+# end
+
 
